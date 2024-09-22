39eb0dfa6c97e7e2c8e201f5d533a557e83df966
Pablo Maribondo <pablomgvieira@gmail.com>
2024-09-22 12:04:14 -0300

    feat: create player

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ canvas.height = window.innerHeight;
 
 context.fillStyle = "black";
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+class Player {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    context.moveTo(this.position.x + 30, this.position.y);
+    context.lineTo(this.position.x - 10, this.position.y - 10);
+    context.lineTo(this.position.x - 10, this.position.y + 10);
+    context.closePath();
+
+    context.strokeStyle = "white";
+    context.stroke();
+  }
+}
+
+const player = new Player({
+  position: { x: canvas.width / 2, y: canvas.height / 2 },
+  velocity: { x: 0, y: 0 },
+});
+player.draw();
