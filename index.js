0b707353240a3a2822be3d9cba9e3aec1d52a83a
Pablo Maribondo <pablomgvieira@gmail.com>
2024-09-21 23:41:02 -0300

    project setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+context.fillStyle = 'black'
+context.fillRect(0, 0, canvas.width, canvas.height);
