459debba07c590b342dd0579e6988faea8ed97ba
Pablo Maribondo <pablomgvieira@gmail.com>
2024-09-22 11:46:49 -0300

    refactor: organize index.html and create a separate style file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.fillStyle = 'black'
+context.fillStyle = "black";
 context.fillRect(0, 0, canvas.width, canvas.height);
